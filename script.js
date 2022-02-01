7e7e64e64d8fe07535a4709bd947bc57f238e39c
Evgeny Grabun <grabun96@mail.ru>
2022-02-01 16:17:33 +0300

    выполнение четвертого урока

--- script.js
+++ script.js
@@ -9,33 +9,60 @@ let service1 = prompt("Какой дополнительный тип услуг
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
 
-console.log(typeof(title));
-
-console.log(typeof(fullPrice));
-
-console.log(typeof(adaptive));
-
-console.log(screens.length);
-
-console.log("Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани");
-
-console.log("Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
-
-console.log(screens.toLowerCase().split(", "));
-
-console.log(fullPrice * (rollback / 10000));
-
-console.log(servicePercentPrice);
-
-if (fullPrice > 30000) {
-  console.log("Даем скидку в 10%");
-} else if (fullPrice > 15000 && fullPrice <= 30000) {
-  console.log("Даем скидку в 5%");
-} else if (fullPrice >= 0 && fullPrice <= 15000) {
-  console.log("Скидка не предусмотрена");
-} else {
-  console.log("Что то пошло не так");
+let showTypeOf = function(variable) {
+  console.log(variable, typeof variable);
 }
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+let getRollbackMessage = function(price) {
+  if (price > 30000) {
+    return "Даем скидку в 10%";
+  } else if (price > 15000 && price <= 30000) {
+    return "Даем скидку в 5%";
+  } else if (price >= 0 && price <= 15000) {
+    return "Скидка не предусмотрена";
+  } else {
+    return "Что то пошло не так";
+  }
+}
+
+let getAllServicePrices = function() {
+
+  return servicePrice1 + servicePrice2;
+}
+
+allServicePrices = getAllServicePrices()
+
+function getFullPrice() {
+  return screenPrice + allServicePrices;
+
+}
+
+fullPrice = getFullPrice()
+
+let getTitle = function(){
+  title = title.trim();
+
+ return title[0].toUpperCase() + title.slice(1).toLowerCase();
+}
+
+getTitle()
+
+let getServicePercentPrices = function() {
+
+  return Math.ceil(fullPrice - fullPrice * (rollback / 100));
+}
+
+servicePercentPrice = getServicePercentPrices()
+
+
+console.log(screens.split(", "));
+console.log(getRollbackMessage(fullPrice));
+console.log(getServicePercentPrices());
